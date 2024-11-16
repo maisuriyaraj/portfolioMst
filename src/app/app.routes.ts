d3971678fe8b4b09b8d4589b7aed824115d1a504
maisuriyaraj <maisuriyaraj664@gmail.com>
2024-11-16 18:56:35 +0530

    Commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './pages/public/login/login.component';
+import { RegisterComponent } from './pages/public/register/register.component';
+import { ForgotPasswordComponent } from './pages/public/forgotPassword/forgotPassword.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path : '',
+        redirectTo : 'Signin',
+        pathMatch : 'full'
+    },
+    {   
+        path : 'Signin',
+        component : LoginComponent
+    },
+    {
+        path:'Register',
+        component : RegisterComponent
+    },
+    {
+        path:'forgotPassword',
+        component : ForgotPasswordComponent
+    }
+];
